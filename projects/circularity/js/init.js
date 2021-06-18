d20b10286b1dd7363a5ae91027bbd42249cad4a4
sherwinbynum <30sttdmpv@gmail.com>
2021-06-18 14:07:06 +0000

    message

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,26 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;			// variable to hold a single circle when creating circles / iterating
+        var circles = [];	// variable to store all circles in one Array
 
         // TODO 2 : Create a function that draws a circle 
+       
+        // Code to draw a circle
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        circles.push(circle); 
+
+        // TODO 3 / 7 : Call the drawCircle() function 
         
-
-        // TODO 3 / 8 : Call the drawCircle() function 
-
-
+        /* Your Function Calls HERE */
+       drawCircle();
+       drawCircle();
+       drawCircle();
+       drawCircle();
+       drawCircle();
+        
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,11 +51,21 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            
+            physikz.updatePosition( /* Your Bracket Notation HERE */ );
+	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
+	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
+	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
+	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
-
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
             // TODO 9 : Iterate over the array
            
             
